a463ce388534a68fd89e9d0de345a53fa04cf4fb
Anthony Garayua <anthonygjr13@gmail.com>
2023-02-21 16:33:02 -0600

    Added amazon as an option

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
-const CONTEXT_MENU_ID = "MY_CONTEXT_MENU";
-const CONTEXT_MENU_ID2 = "MY_CONTEXT_MENU2";
+const SPOTIFY_CONTEXT_MENU_ID = "SPOTIFY_CONTEXT_MENU";
+const YOUTUBE_CONTEXT_MENU_ID = "YOUTUBE_CONTEXT_MENU";
+const AMAZON_CONTEXT_MENU_ID = "AMAZON_CONTEXT_MENU";
 
 function getword(info,tab) {
-  if (info.menuItemId !== CONTEXT_MENU_ID || CONTEXT_MENU_ID2)
-  {
-    return;
-  }
-  if (info.menuItemId == CONTEXT_MENU_ID) {
+  
+  if (info.menuItemId == SPOTIFY_CONTEXT_MENU_ID) {
     chrome.tabs.create({  
       url: "https://open.spotify.com/search/" + info.selectionText
     });
   }
   
-  if (info.menuItemId == CONTEXT_MENU_ID2) {
+  if (info.menuItemId == YOUTUBE_CONTEXT_MENU_ID) {
     chrome.tabs.create({  
       url: "https://www.youtube.com/results?search_query=" + info.selectionText
     });
   }
   
+  if (info.menuItemId == AMAZON_CONTEXT_MENU_ID) {
+    chrome.tabs.create({  
+      url: "https://www.amazon.com/s?k=" + info.selectionText
+    });
+  }
+
 }
 chrome.contextMenus.create({
   title: "Search '%s' on Spotify", 
   contexts:["selection"], 
-  id: CONTEXT_MENU_ID
+  id: SPOTIFY_CONTEXT_MENU_ID
 });
 
 chrome.contextMenus.create({
   title: "Search '%s' on YouTube", 
   contexts:["selection"], 
-  id: CONTEXT_MENU_ID2
+  id: YOUTUBE_CONTEXT_MENU_ID
+});
+
+chrome.contextMenus.create({
+  title: "Search '%s' on Amazon", 
+  contexts:["selection"], 
+  id: AMAZON_CONTEXT_MENU_ID
 });
 chrome.contextMenus.onClicked.addListener(getword)
